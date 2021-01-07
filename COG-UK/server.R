a8d26b81c6afc6d18a0a813838f66aafbb10b52b
Derek.Wright@glasgow.ac.uk <Derek.Wright@glasgow.ac.uk>
2021-01-07 18:22:24 +0000

    Display report table 1

--- COG-UK/server.R
+++ COG-UK/server.R
@@ -1,61 +1,55 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 
 shinyServer(function(input, output, session) {
 
-    output$mutation_time <- renderPlot({
+    # output$mutation_time <- renderPlot({
+    #     
+    #     mutations %>% filter(gene == input$gene 
+    #                          & variant == input$variant
+    #                          & sample_date >= input$date_range[1] 
+    #                          & sample_date <= input$date_range[2]) %>% ggplot(aes(x = sample_date)) + geom_bar() + theme_minimal()
+    # })
+    # 
+    # output$raw_mutations <- DT::renderDataTable(
+    #     mutations %>% 
+    #     filter(gene == input$gene 
+    #            & position == input$position 
+    #            & variant == input$variant 
+    #            & sample_date >= input$date_range[1] 
+    #            & sample_date <= input$date_range[2])
+    # )
+    
+    output$raw_database <- renderTable(
+        database %>% 
+            slice_max(`numSeqs UK`, n = 15) %>% 
+            select(replacement, `numSeqs UK`, `numSeqs UK 28 days`, `numSeqs Eng 28 days`, `numSeqs Scotland 28 days`, `numSeqs Wales 28 days`, `numSeqs NI 28 days`)
         
-        mutations %>% filter(gene == input$gene 
-                             & variant == input$variant
-                             & sample_date >= input$date_range[1] 
-                             & sample_date <= input$date_range[2]) %>% ggplot(aes(x = sample_date)) + geom_bar() + theme_minimal()
-    })
-    
-    output$raw_mutations <- DT::renderDataTable(
-        mutations %>% 
-        filter(gene == input$gene 
-               & position == input$position 
-               & variant == input$variant 
-               & sample_date >= input$date_range[1] 
-               & sample_date <= input$date_range[2])
     )
     
-    output$raw_database <- DT::renderDataTable(
-        database
-    )
-    
-    output$summary <- renderTable({
-        mutations %>% 
-            filter(gene == input$gene 
-                   & position == input$position 
-                   & variant == input$variant
-                   & sample_date >= input$date_range[1] 
-                   & sample_date <= input$date_range[2]) %>% count(country)
-    })
+    # output$summary <- renderTable({
+    #     mutations %>% 
+    #         filter(gene == input$gene 
+    #                & position == input$position 
+    #                & variant == input$variant
+    #                & sample_date >= input$date_range[1] 
+    #                & sample_date <= input$date_range[2]) %>% count(country)
+    # })
     
     # database %>% 
     #     slice_max(`numSeqs UK`, n = 15) %>% 
     #     select(replacement, `numSeqs UK`, `numSeqs UK 28 days`, `numSeqs Eng 28 days`, `numSeqs Scotland 28 days`, `numSeqs Wales 28 days`, `numSeqs NI 28 days`)
     
     
-
-    observeEvent(input$gene, {
-        updateSelectInput(session, "position",
-                          choices = mutations %>% filter(gene == input$gene) %>% distinct(position) %>% arrange(position))
-    })
-    
-    observeEvent(input$position, {
-        updateSelectInput(session, "variant",
-                          choices = mutations %>% filter(gene == input$gene & position == input$position) %>% distinct(variant) %>% arrange(variant))
-    })
+# 
+#     observeEvent(input$gene, {
+#         updateSelectInput(session, "position",
+#                           choices = mutations %>% filter(gene == input$gene) %>% distinct(position) %>% arrange(position))
+#     })
+#     
+#     observeEvent(input$position, {
+#         updateSelectInput(session, "variant",
+#                           choices = mutations %>% filter(gene == input$gene & position == input$position) %>% distinct(variant) %>% arrange(variant))
+#     })
     
 })
--- COG-UK/ui.R
+++ COG-UK/ui.R
@@ -6,39 +6,45 @@ library(shinydashboard)
 dashboardPage(
     dashboardHeader(title = "COG-UK"),
     dashboardSidebar(
-        selectInput("gene", "Gene:", mutations %>% distinct(gene) %>% arrange(gene), selected = "S"),
-        selectInput("position", "Position:", mutations %>% distinct(position) %>% arrange(position)),
-        selectInput("variant", "Variant:", mutations %>% distinct(variant) %>% arrange(variant)),
-        dateRangeInput('date_range',
-                       label = 'Date range:',
-                       start = min(mutations$sample_date), end = max(mutations$sample_date)
-        ),
+        # selectInput("gene", "Gene:", mutations %>% distinct(gene) %>% arrange(gene), selected = "S"),
+        # selectInput("position", "Position:", mutations %>% distinct(position) %>% arrange(position)),
+        # selectInput("variant", "Variant:", mutations %>% distinct(variant) %>% arrange(variant)),
+        # dateRangeInput('date_range',
+        #                label = 'Date range:',
+        #                start = min(mutations$sample_date), end = max(mutations$sample_date)
+        # ),
         
         sidebarMenu(
-            menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-            menuItem("Raw Data", tabName = "raw", icon = icon("table"))
+            menuItem("Report", tabName = "report", icon = icon("table"))
+            # menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+            # menuItem("Raw Data", tabName = "raw", icon = icon("database"))
         )
     ),
     dashboardBody(
         tabItems(
-            # First tab content
-            tabItem(tabName = "dashboard",
+            tabItem(tabName = "report",
                     fluidRow(
-                        box(plotOutput("mutation_time"), width = 12),
-                        box(h4("Counts of variant by country"),
-                            tableOutput("summary"), width = 6)
-                    ), 
-            ),
-            
-            # Second tab content
-            tabItem(tabName = "raw",
-                    h2("Raw Data"), 
-                    h3("Mutations"),
-                    DT::dataTableOutput("raw_mutations"), 
-                    h3("Spike Database"),
-                    DT::dataTableOutput("raw_database")
+                        tableOutput("raw_database")
+                    )
             )
-        )
-    )
+        ) # end tabItems
+            
+            # tabItem(tabName = "dashboard",
+            #         fluidRow(
+            #             box(plotOutput("mutation_time"), width = 12),
+            #             box(h4("Counts of variant by country"),
+            #                 tableOutput("summary"), width = 6)
+            #         ), 
+            # ),
+            
+            # tabItem(tabName = "raw",
+            #         h2("Raw Data"), 
+            #         h3("Mutations"),
+            #         DT::dataTableOutput("raw_mutations"), 
+            #         h3("Spike Database"),
+            #         DT::dataTableOutput("raw_database")
+            # )
+     #   )
+    ) # end dashboardBody
 )
 
