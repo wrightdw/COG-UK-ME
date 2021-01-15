4835e2ecde7332bfb1998dfba6e4c4f39c64a07f
Derek.Wright@glasgow.ac.uk <Derek.Wright@glasgow.ac.uk>
2021-01-15 11:44:39 +0000

    Report Table 4

--- COG-UK/server.R
+++ COG-UK/server.R
@@ -3,6 +3,33 @@ library(tidyverse)
 
 lineages_t2 <- c("B.1", "B.1.177", "B.1.141", "B.1.258", "B.1.1", "B.1.1.7", "B.1.1.70", "B.1.351", "B.1.1.298")
 lineages_t3 <- c("B.1.1.7", "B.1.351", "P.1")
+escape_t4 <- c(
+"G446V",
+"L452R",
+"E484Q",
+"K444R",
+"E484K",
+"Y508H",
+"N440K",
+"L455F",
+"A831V",
+"A475V",
+"F490S",
+"V483A",
+"R346K",
+"K378N",
+"K444N",
+"G446S",
+"N450D",
+"K150R",
+"R346S",
+"K150T",
+"V445A",
+"G446A",
+"Y449H",
+"E484A",
+"E484R",
+"Q493R")
 
 shinyServer(function(input, output, session) {
 
@@ -86,22 +113,6 @@ shinyServer(function(input, output, session) {
     })
     
     output$table_3 <- renderTable({
-        
-        # key_mutations <-
-        #     sum_key_mutations_uk() %>%
-        #     filter(lineage %in% key_lineages) %>%
-        #     select(lineage, sequences)
-        # 
-        # key_mutations_28  <- sum_key_mutations_uk(date_from = sample_date_28) %>% 
-        #     filter(lineage %in% key_lineages) %>% 
-        #     select(lineage, sequences) %>% 
-        #     rename(sequences_28 = sequences)
-        # 
-        # key_mutations %>% inner_join(key_mutations_28)
-        
-        # filter(lineage == x | ) %>% 
-        # 
-
         key_lineages <- 
         lapply(lineages_t3, function(x){
             sum_key_mutations_uk() %>% 
@@ -117,13 +128,23 @@ shinyServer(function(input, output, session) {
                     filter(lineage == x | str_detect(lineage, sublineage_regex(x)) ) %>% 
                     select(-lineage) %>%
                     summarise(sequences_sum = sum(sequences)) %>% 
-                    mutate(lineage = x, .before = 1)
-            }) %>% bind_rows() %>% rename(sequences_sum_28 = sequences_sum)
+                    mutate(lineage = x, .before = 1) %>% rename(sequences_sum_28 = sequences_sum)
+            }) %>% bind_rows() 
         
-        inner_join(key_lineages, key_lineages_28)
-        
+        inner_join(key_lineages, key_lineages_28) %>% rename(`Variant/ lineage` = lineage,	
+                                                             `Cumulative sequences in UK` = sequences_sum,	 
+                                                             `Sequences over 28 days` = sequences_sum_28)
     })
-    output$table_4 <- renderTable({})
+    output$table_4 <- renderTable({
+        database %>%
+            filter(replacement %in% escape_t4) %>%
+            select(replacement, `numSeqs UK`, `numSeqs UK 28 days`) %>%  
+            mutate(replacement = factor(replacement, levels = escape_t4)) %>%
+            arrange(replacement) %>% 
+            rename(`Amino acid replacement` = replacement, 
+                   `Cumulative sequences in UK` = `numSeqs UK`,
+                   `Sequences over 28 days` = `numSeqs UK 28 days`)
+    })
     
     # output$mutation_time <- renderPlot({
     #     
