383c95e03a7dd314eba401ed3a533a892d69d7b0
Derek Wright <Derek.Wright@glasgow.ac.uk>
2021-04-22 12:51:22 +0100

    Update to 21st April 2021

--- COG-UK/global.R
+++ COG-UK/global.R
@@ -3,7 +3,7 @@ library(lubridate)
 library(magrittr)
 library(RColorBrewer)
 
-dataset_date <- ymd("2021-04-19") #TODO derive from directory name
+dataset_date <- ymd("2021-04-21") #TODO derive from directory name
 
 database <- str_c(dataset_date, "/database.rds") %>% read_rds # spike database
 consortium_uk <- str_c(dataset_date, "/consortium_uk.rds") %>% read_rds
