2c22fa17ce2b9c5ba6ad587a2b02e2a47faf35da
Derek.Wright@glasgow.ac.uk <Derek.Wright@glasgow.ac.uk>
2021-07-08 18:59:22 +0100

    Create run_mutations.sh

---
+++ run_mutations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+date
+cd /cephfs/covid/bham/climb-covid19-wrightd/COG-UK-Dashboard
+source /cephfs/covid/bham/climb-covid19-wrightd/miniconda3/etc/profile.d/conda.sh
+conda activate r_env
+Rscript mutations.R
