a38f470713320535bb1f4758a62af4a56c6f7252
Derek.Wright@glasgow.ac.uk <Derek.Wright@glasgow.ac.uk>
2021-07-08 19:19:37 +0100

    Set timezone to London

--- run_mutations.sh
+++ run_mutations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-date
+TZ="Europe/London" date
 cd /cephfs/covid/bham/climb-covid19-wrightd/COG-UK-Dashboard
 source /cephfs/covid/bham/climb-covid19-wrightd/miniconda3/etc/profile.d/conda.sh
 conda activate r_env
