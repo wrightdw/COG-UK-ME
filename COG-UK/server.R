208893db08a036b9a7f7631cf1c90ea435d6640c
Derek.Wright@glasgow.ac.uk <Derek.Wright@glasgow.ac.uk>
2021-11-23 16:44:45 +0000

    Convert Delta sublineages to B.1.617.2 for NUTS1 counts

--- COG-UK/server.R
+++ COG-UK/server.R
@@ -1065,9 +1065,9 @@ shinyServer(function(input, output, session) {
       # generate plot
       gg <- ggplot() + geom_polygon(data = df, aes(x = long, y = lat, group = group, fill = value), color = "#FFFFFF", size = 0.25)
       gg <- gg + scale_fill_gradient2(low = "blue", mid = "red", high = "yellow", na.value = "white")
-      gg <- gg + coord_fixed(1)
+      gg <- gg + coord_fixed(1) # TODO conic conformal map projection?
       gg <- gg + theme_minimal()
-      gg <- gg +  scale_fill_viridis(name= c, limits = c(0,max_val)) 
+      gg <- gg + scale_fill_viridis(name= c, limits = c(0,max_val)) 
       gg <- gg + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = 'right')
       gg <- gg + theme(axis.title.x=element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
       gg <- gg + theme(axis.title.y=element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
