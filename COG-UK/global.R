13ce141d9db88b6dc91f13ceab8f6d7e6c5b4de3
alecarabo <73174407+alecarabo@users.noreply.github.com>
2022-02-10 16:43:12 +0000

    Insertion download

--- COG-UK/global.R
+++ COG-UK/global.R
@@ -44,6 +44,7 @@ deletions <- str_c(dataset_date, "/deletions.rds") %>% read_rds # deletions (gen
 database_deletions <- read_rds(str_c(dataset_date, "/database_deletions.rds"))
 deletions_mapping <- read_rds(str_c(dataset_date, "/deletions_mapping.rds"))
 database_insertions <- read_rds(str_c(dataset_date, "/database_insertions.rds"))
+insertions_mapping <- read_rds(str_c(dataset_date, "/insertions_mapping.rds"))
 
 source("helpers.R")
 
@@ -86,7 +87,7 @@ mutations_s_uk <-
   select(-gene) %>% 
   mutate(across(c(variant, position), fct_drop)) # drop non-spike mutations from factor levels
 
-mutations_uk %<>% bind_rows(deletions_mapping)
+mutations_uk %<>% bind_rows(deletions_mapping) %>% bind_rows(insertions_mapping)
 
 sample_date_28 <- max(consortium_uk$sample_date) - days(27) # calculate 28 day period up to and including latest sample date
 
