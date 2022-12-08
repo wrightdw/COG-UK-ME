0745913e34b8e31e777e4ce964b5cf531022cbd4
Derek Wright <wrightdw@users.noreply.github.com>
2022-12-08 17:20:27 +0000

    Move epitope GFF file and index to sars2

--- COG-UK/server.R
+++ COG-UK/server.R
@@ -717,8 +717,8 @@ shinyServer(function(input, output, session) {
     )
     
     epitopes_track <- track_feature(
-      "https://bioinformatics.cvr.ac.uk/spike_epitopes_sorted_2022-11-28.gff3.gz",
-      # "https://sars2.cvr.gla.ac.uk/cog-uk/spike_epitopes_sorted_2022-11-27.gff3.gz",
+      # "https://bioinformatics.cvr.ac.uk/spike_epitopes_sorted_2022-11-28.gff3.gz",
+      "https://sars2.cvr.gla.ac.uk/cog-uk/spike_epitopes_sorted_2022-11-28.gff3.gz",
       assembly)
     
     # create the tracks array to pass to browser
