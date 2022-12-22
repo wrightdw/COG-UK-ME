61e2d2025b378f78cae43696aa7c60dde9fe15af
Derek Wright <wrightdw@users.noreply.github.com>
2022-12-22 15:34:26 +0000

    Sort and index GFF3 then move to Shiny server

--- run_mutations.sh
+++ run_mutations.sh
@@ -6,8 +6,20 @@ conda activate r_env
 Rscript mutations.R
 if [ $? -eq 0 ]
 	then
+   		
 		TODAY=$(TZ="Europe/London" date +%F)
+		
+		echo "Sorting and indexing GFF3 file"
+		conda activate gt_env
+		cd "COG-UK/${TODAY}"
+		gt gff3 -sortlines -tidy spike_epitopes.gff3 >  spike_epitopes_sorted.gff3
+		bgzip spike_epitopes_sorted.gff3
+		tabix -p gff spike_epitopes_sorted.gff3.gz
+		rm spike_epitopes.gff3
+
 		echo "Creating archive ${TODAY}.tar.gz"
+		cd /cephfs/covid/bham/climb-covid19-wrightd/COG-UK-Dashboard
 		tar -czvf "${TODAY}.tar.gz" "COG-UK/${TODAY}" COG-UK/vui_voc.rds
+		TZ="Europe/London" date	
 		echo "Done"
 fi
