84dd5b219f04721bb80945cd1ba4487814998461
Derek Wright <wrightdw@users.noreply.github.com>
2022-12-22 15:34:26 +0000

    Sort and index GFF3 then move to Shiny Server

--- stop_start_mutations.sh
+++ stop_start_mutations.sh
@@ -6,10 +6,15 @@ if [ -d ./${TODAY} ];  then
     echo "$PWD/${TODAY}" exists
 else
 	if sudo -u dw73x /home/dw73x/update_mutations.sh;  then
+		echo "Moving GFF3 and index to /srv/shiny-server/jbrowser/"
+		mv -f /home/dw73x/COG-UK/${TODAY}/spike_epitopes_sorted.gff3.gz* /srv/shiny-server/jbrowser/
+		
 		echo "Moving /home/dw73x/COG-UK/${TODAY} to ${PWD}"
 	    	mv /home/dw73x/COG-UK/${TODAY} .
+
 		echo "Moving /home/dw73x/COG-UK/vui_voc.rds to ${PWD}"
 	    	mv /home/dw73x/COG-UK/vui_voc.rds .
+
 		TZ="Europe/London" date
 		echo "Done"
 	else
