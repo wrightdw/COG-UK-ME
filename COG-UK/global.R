ea5f6973c3aacf44c4f9ec6fc99afa49983ba4f3
Orges Koci <koci.orges@hotmail.com>
2023-01-18 17:03:44 +0000

    Update global.R to reflect changes relative to the new ESM-2 dataset and integrate the dataset with Sergei's entropy

--- COG-UK/global.R
+++ COG-UK/global.R
@@ -55,11 +55,15 @@ database_insertions <- read_rds(str_c(dataset_date, "/database_insertions.rds"))
 source("helpers.R")
 
 dms_antigenic <- read_rds("dms_integrated_data@13.rds")
-dms_antigenic %<>% select(-c("real", "convergent", "bloom_bind_avg", "bloom_expr_avg", "color")) %>%
+dms_antigenic %<>% select(-c("grammatical_rank")) %>% # grammatical_rank temporarily excluded until correct data become available
   filter(!is.na(semantic_score)) %>%
   filter(!is.na(grammaticality)) %>%
-  filter(!is.na(evolutionary_index)) %>%
-  filter(!is.na(entropy))
+  filter(!is.na(evolutionary_index))
+
+sergei_entropies_spike_all = read.csv("sergei_entropies_spike_coordinates.csv", sep=";")
+sergei_entropies_spike_all$position = as.numeric(rownames(sergei_entropies_spike_all))
+dms_antigenic <- dms_antigenic %>% left_join(sergei_entropies_spike_all[,c("entropy", "position"), ], by="position")
+dms_antigenic %<>% filter(!is.na(entropy))
 
 ref_wuhan = paste("MFVFLVLLPLVSSQCVNLTTRTQLPPAYTNSFTRGVYYPDKVFRSSVLHSTQDLFLPFFSNVTWFHAIHVSGTNGTKRFDNPVLPFNDGVYFASTEKSNIIR", 
                   "GWIFGTTLDSKTQSLLIVNNATNVVIKVCEFQFCNDPFLGVYYHKNNKSWMESEFRVYSSANNCTFEYVSQPFLMDLEGKQGNFKNLREFVFKNIDGYFKIYS",
